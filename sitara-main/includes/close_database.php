adec1e011dadd91383f3e61b70921b22b0e49fc1
Asif Aslam <asifaslamaimviz@gmail.com>
2022-11-25 03:48:16 +0500

    first commit

--- sitara-main/includes/close_database.php
+++
@@ -1,5 +0,0 @@
-<?php
-	//error_reporting(0);
-	include($_SERVER['DOCUMENT_ROOT'].'/sitara/includes/variables.php');
-	$connect->close();
-?>
--- sitara-main/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["username"]);
-unset($_SESSION["password"]);
-unset($_SESSION["userid"]);
-unset($_SESSION["name"]);
-header("Location:index.php");
-?>
